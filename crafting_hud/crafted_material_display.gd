b86b456f9e72b4a4046a31a709c817bc7f7630d8
DaVinci789 <jaden.manacsa1228@gmail.com>
2020-03-17 16:40:39 -0500

    Center crafted material for the most part

--- crafting_hud/crafted_material_display.gd
+++ crafting_hud/crafted_material_display.gd
@@ -11,5 +11,12 @@ func spawn_crafted_material(material_to_craft: String) -> void:
 	var crafted_material: TextureRect = material_scene.instance()
 	crafted_material.texture = crafted_material_textures[material_to_craft]
 	crafted_material.rect_scale = Vector2(1,1)
+	
+	# centers the material.
+	# magic numbers derived from centering the $Label child
+	crafted_material.margin_left = 30
+	crafted_material.margin_top  = 10
+	crafted_material.margin_right = -30
+	crafted_material.margin_bottom = 10
 	add_child(crafted_material)
 	pass
