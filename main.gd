881f524a3341409ed32b91927ee1af75ba567992
DaVinci789 <jaden.manacsa1228@gmail.com>
2020-03-26 00:00:42 -0500

    Add room scenes

--- main.gd
+++ main.gd
@@ -1,37 +1,65 @@
 extends Node2D
 
 enum Directions {
-    NORTH,
-    SOUTH,
-    EAST,
-    WEST,
+	NORTH,
+	SOUTH,
+	EAST,
+	WEST,
 }
 
 # 12 * 16 == 192
-const ROOM_WIDTH = 192
-const ROOM_HEIGHT = 192
+const ROOM_AMOUNT = 4
+const TILE_SIZE   = 16
 
 const order_room_scene = preload("res://rooms/order_room/order_room.tscn")
 
 const room_scenes = {
-    "order_room": preload("res://rooms/order_room/order_room.tscn")
+	"dining_room1": preload("res://rooms/dining_room1/dining_room1.tscn"),
+	"dining_room2": preload("res://rooms/dining_room2/dining_room2.tscn"),
+	"dining_room3": preload("res://rooms/dining_room3/dining_room3.tscn"),
 }
 
-func _ready():
-    randomize()
-    add_child(order_room_scene.instance())
+var rooms := []
 
-    var next_room: TileMap = room_scenes[room_scenes.keys()[randi() % room_scenes.keys().size()]].instance()
-    match Directions.values()[randi() % Directions.values().size()]:
-        Directions.NORTH:
-            next_room.position = Vector2(0,-1) * ROOM_HEIGHT
-        Directions.SOUTH:
-            next_room.position = Vector2(0,1) * ROOM_HEIGHT
-        Directions.EAST:
-            next_room.position = Vector2(1,0) * ROOM_WIDTH
-        Directions.WEST:
-            next_room.position = Vector2(-1,0) * ROOM_WIDTH
-        _:
-            print("Uh oh...")
-    add_child(next_room)
-    move_child($player, self.get_children().size())
+func _ready():
+	generate_rooms()
+
+func generate_rooms():
+	randomize()
+	add_child(order_room_scene.instance())
+
+	var current_direction := Vector2.ZERO
+	rooms.append(current_direction)
+
+	for i in ROOM_AMOUNT:
+		var next_room := current_direction
+		match Directions.values()[randi() % Directions.values().size()]:
+			Directions.NORTH:
+				next_room += Vector2(0,-1)
+			Directions.SOUTH:
+				next_room += Vector2(0,1)
+			Directions.EAST:
+				next_room += Vector2(1,0)
+			Directions.WEST:
+				next_room += Vector2(-1,0)
+			_:
+				print("Uh oh...")
+		rooms.append(next_room)
+		current_direction = next_room
+	
+	for room_position in rooms:
+		# different for starting room
+		if room_position == Vector2(0,0):
+			var current_room: TileMap = order_room_scene.instance()
+			current_room.position = Vector2(0,0)
+			add_child(current_room)
+			continue
+		# get random room from room_scenes
+		var current_room: TileMap = room_scenes[room_scenes.keys()[randi() % room_scenes.keys().size()]].instance()
+		# scales from local unit vector coords to global coords
+		current_room.position = room_position * (current_room.room_width * TILE_SIZE) 
+		add_child(current_room)
+
+	# move player node on top
+	move_child($player, self.get_children().size())
+	pass
--- rooms/room_base.gd
+++ rooms/room_base.gd
@@ -1,4 +1,32 @@
 extends TileMap
 
-export (int) var room_width := 0
-export (int) var room_height := 0
+export (int) var room_width := 16
+export (int) var room_height := 16
+
+var wall_length := 12
+var wall_height := 2
+
+func _ready():
+	set_walls(["north"])
+
+func set_walls(exceptions: Array):
+		#for each side, construct wall unless "north", "south", "east", or "west" is in exceptions
+		var walls_to_create = ["north", "south", "east", "west"]
+		for wall in walls_to_create:
+				if wall in exceptions:
+						walls_to_create.remove(walls_to_create.find(wall))
+		for wall in walls_to_create:
+				for length in wall_length:
+						for height in wall_height:
+								match wall:
+										"north":
+												# y-offset, x-offset
+												set_cell(length + 2, height, 1)
+										"south":
+												set_cell(length + 2, height + room_height, 1)
+										"east":
+												# x-offset, y-offset
+												set_cell(height, length + 2, 1)
+										"west":
+												set_cell(height + room_width - 2, length + 2, 1)
+		update_bitmask_region(Vector2(0,0), map_to_world(Vector2(room_width, room_height)))
