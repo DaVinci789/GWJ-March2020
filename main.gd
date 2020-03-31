ca0da9698ad245b375dca909b02c6d1cd65074d2
DaVinci789 <jaden.manacsa1228@gmail.com>
2020-03-31 07:25:13 -0500

    Press "B" to generate_rooms()

--- main.gd
+++ main.gd
@@ -7,7 +7,7 @@ enum Directions {
 	WEST,
 }
 
-# 12 * 16 == 192
+# this is in addition to the starting room
 const ROOM_AMOUNT = 4
 const TILE_SIZE   = 16
 
@@ -30,9 +30,23 @@ func _ready():
 	var starting_room_map: TileMap = rooms[0].tilemap
 	$player.position = Vector2(starting_room_map.room_width / 2, starting_room_map.room_height / 2) * TILE_SIZE
 
+func _process(_delta):
+	if Input.is_action_just_pressed("reset"):
+		generate_rooms()
+		move_child($player, self.get_children().size())
+
 func generate_rooms():
+	# resets rooms for each time this function is called
+	for node in get_children():
+		if node.is_in_group("room"):
+			node.free()
+	# godot is being weird. won't free the order room node
+	for node in get_children():
+		if "order_room" in node.name:
+			node.free()
+	rooms = []
+
 	randomize()
-	add_child(order_room_scene.instance())
 
 	var room_positions = []
 
@@ -42,16 +56,7 @@ func generate_rooms():
 	# chooses random exit for each room
 	for i in ROOM_AMOUNT:
 		var next_room := current_direction
-		var exit = Directions.values()[randi() % Directions.values().size()]
-		match exit:
-			Directions.NORTH:
-				next_room += Vector2(0,-1)
-			Directions.SOUTH:
-				next_room += Vector2(0,1)
-			Directions.EAST:
-				next_room += Vector2(1,0)
-			Directions.WEST:
-				next_room += Vector2(-1,0)
+		next_room = generate_random_room_location(next_room, room_positions)
 		room_positions.append(next_room)
 		current_direction = next_room
 	
@@ -59,15 +64,16 @@ func generate_rooms():
 		# different for starting room
 		if room_position == Vector2(0,0):
 			var current_room: TileMap = order_room_scene.instance()
+			current_room.add_to_group("room")
 			current_room.position = Vector2(0,0)
 			add_child(current_room)
-			#rooms[room_position] = current_room
 			rooms.append({"position": room_position, "tilemap": current_room})
 			continue
 		# get random room from room_scenes
 		var current_room: TileMap = room_scenes[room_scenes.keys()[randi() % room_scenes.keys().size()]].instance()
 		# scales from local unit vector coords to global coords
 		current_room.position = room_position * (current_room.room_width * TILE_SIZE) 
+		current_room.add_to_group("room")
 
 		rooms.append({"position": room_position, "tilemap": current_room})
 		add_child(current_room)
@@ -97,15 +103,21 @@ func generate_rooms():
 		var room_tilemap: TileMap = room.get("tilemap")
 		room_tilemap.set_walls(room_tilemap.exits)
 
-func get_opposing_exit(exit: String) -> String:
+# generates a valid room position
+# that is, a room position that is not in previous_rooms
+func generate_random_room_location(location: Vector2, previous_rooms: Array) -> Vector2:
+	var exit = Directions.values()[randi() % Directions.values().size()]
+	var next_room = location
 	match exit:
-		"north":
-			return "south"
-		"south":
-			return "north"
-		"east":
-			return "west"
-		"west":
-			return "east"
-		_:
-			return "None"
+		Directions.NORTH:
+			next_room += Vector2(0,-1)
+		Directions.SOUTH:
+			next_room += Vector2(0,1)
+		Directions.EAST:
+			next_room += Vector2(1,0)
+		Directions.WEST:
+			next_room += Vector2(-1,0)
+	if next_room in previous_rooms:
+		return generate_random_room_location(location, previous_rooms)
+	else:
+		return next_room
